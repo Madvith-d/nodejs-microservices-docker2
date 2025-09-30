86a87e68dd7995f1d72706a10389046d4b16e475
Madvith <madvith@example.com>
2025-09-30 15:28:37 +0530

    coonected mongoDb via docker container ,

--- userService/index.js
+++ userService/index.js
@@ -1,15 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
+import connectDb from "./db/connectDb.js";
+import UserModel from "./models/UserModel.js";
 const PORT = 3000;
 const app  = express();
 
+connectDb();
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/users', async (req, res) => {
+    const {name,email}= req.body;
+    if(!name || !email){
+        return res.status(400).json({message:"Name and email are required"});
+    }
+    try {
+        const user = new UserModel({name,email});
+        await user.save();
+        return res.status(201).json({message:"User created successfully",user});
+    
+    } catch (error) {
+        return res.status(500).json({message:"Internal server error" , error:error.message});
+    }
+   
+    
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-    
