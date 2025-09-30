a25e304545fb1ebac68ea09b22f7848d306bf545
Madvith <madvith@example.com>
2025-09-30 15:28:10 +0530

    Remove node_modules from tracking

---
+++ userService/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+const PORT = 3000;
+const app  = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+    
